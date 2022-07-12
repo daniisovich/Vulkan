41503c9e29e1a75f1c9afd9bfce54ab0e59ddd0f
unknown <daniel.preuss93@gmail.com>
2022-07-12 18:57:27 +0200

    Changed formatting of debug messages

--- HelloTriangle/src/vulkan/debug/vulkan_debug.h
+++ HelloTriangle/src/vulkan/debug/vulkan_debug.h
@@ -18,10 +18,10 @@
 			void* p_user_data)
 		{
 			std::cerr << vk::to_string(static_cast<vk::DebugUtilsMessageSeverityFlagBitsEXT>(severity)) << "::" 
-					  << vk::to_string(static_cast<vk::DebugUtilsMessageTypeFlagBitsEXT>(type)) << "\t" 
-					  << "Name\t\t<" << p_callback_data->pMessageIdName << ">\n\t\t" 
-					  << "ID\t\t<" << p_callback_data->messageIdNumber << ">\n\t\t"
-					  << "Message\t\t" << p_callback_data->pMessage << "\n";
+					  << vk::to_string(static_cast<vk::DebugUtilsMessageTypeFlagBitsEXT>(type)) << "\n" 
+					  << "\tName\t\t<" << p_callback_data->pMessageIdName << ">\n" 
+					  << "\tID\t\t<" << p_callback_data->messageIdNumber << ">\n"
+					  << p_callback_data->pMessage << "\n\n";
 			return VK_FALSE;
 		}
 
