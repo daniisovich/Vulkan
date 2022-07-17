ef221e71068106c8359083b086ce4a7358f2d3ae
unknown <daniel.preuss93@gmail.com>
2022-07-17 13:36:30 +0200

    Added vulkan include/lib folders and glslc to vendor folder to remove specific installation path

--- Raytracer/src/vulkan/shaders/compile.bat
+++ Raytracer/src/vulkan/shaders/compile.bat
@@ -1,2 +1,4 @@
-"C:\Program Files (x86)\VulkanSDK\Bin\glslc.exe" glsl/shader.vert -o spir-v/vert.spv
-"C:\Program Files (x86)\VulkanSDK\Bin\glslc.exe" glsl/shader.frag -o spir-v/frag.spv
+set compiler_path="../../../vendor/vulkan/Bin/glslc.exe"
+
+%compiler_path% glsl/shader.vert -o spir-v/vert.spv
+%compiler_path% glsl/shader.frag -o spir-v/frag.spv
